deb0339a7a431b11e0d8bf5a78139a56bbbf6d1c
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-01-23 16:27:40 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
-let creations = document.querySelectorAll(".creation");
+const creations = document.querySelectorAll(".creation");
 
-let creationTitles = document.querySelectorAll(".creation__title");
+const creationTitles = document.querySelectorAll(".creation__title");
 
-let creationBackgrounds = document.querySelectorAll(".creation__background");
+const creationBackgrounds = document.querySelectorAll(".creation__background");
 
 creations.forEach((creation, index) => {
   creation.addEventListener("mouseover", () => {
@@ -17,9 +17,9 @@ creations.forEach((creation, index) => {
   });
 });
 
-let headerMenu = document.querySelector(".header__menu");
+const headerMenu = document.querySelector(".header__menu");
 
-let headerLogo = document.querySelector(".header__logo");
+const headerLogo = document.querySelector(".header__logo");
 
 menuIcon.addEventListener("click", () => {
   if (window.getComputedStyle(headerMenu).display == "none") {
@@ -46,3 +46,23 @@ menuIcon.addEventListener("click", () => {
     menuIcon.style.top = "unset";
   }
 });
+
+/* const headerMenu = document.querySelector(".header__menu");
+const menuIcon = document.querySelector("#menuIcon");
+const headerLogo = document.querySelector(".header__logo");
+const creationTitles = document.querySelectorAll(".creation__title");
+
+menuIcon.addEventListener("click", () => {
+  headerMenu.classList.toggle("show");
+  headerLogo.classList.toggle("fixed");
+  menuIcon.classList.toggle("fixed_icon");
+  creationTitles.forEach((creationTitle) => {
+    creationTitle.classList.toggle("hide");
+  });
+
+  if (window.getComputedStyle(headerMenu).display == 'none') {
+    menuIcon.src = 'images/icon-hamburger.svg'
+  } else {
+    menuIcon.src = "images/icon-close.svg";
+  }
+}); */
